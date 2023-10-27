071c4e6513aa39fffa9b67bab96cceeca08edcde
Lev2607 <levent.o@web.de>
2023-10-27 10:13:52 +0200

    feat: first commitment

--- Aufgabe_27.10.py
+++ Aufgabe_27.10.py
@@ -6,8 +6,8 @@
 gesamt_Kisten = 1000
 kisten_pro_Fahrt = 75
 
-anzahl_Fahrten = gesamt_Kisten / kisten_pro_Fahrt
+anzahl_fahrten = (gesamt_Kisten + kisten_pro_Fahrt - 1) // kisten_pro_Fahrt
 kisten_letzte_fahrt = gesamt_Kisten % kisten_pro_Fahrt
 
-print("Es werden insgesamt", int(anzahl_Fahrten), "Fahrten benötigt.")
+print("Es werden insgesamt", int(anzahl_fahrten), "Fahrten benötigt.")
 print("Die letzte Fahrt enthält", int(kisten_letzte_fahrt), "Kisten.")
